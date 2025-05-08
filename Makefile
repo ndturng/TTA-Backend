bbce41aba688747b2e390b62332a87e84efec98d
Nguyen Duong Trung <trung.nguyenduong.avir@gmail.com>
2025-05-08 03:20:19 +0000

    config database, add make file

---
+++ Makefile
@@ -0,0 +1,4 @@
+run:
+	python manage.py runserver 0.0.0.0:8000
+migrate:
+	python manage.py migrate
--- backend/settings.py
+++ backend/settings.py
@@ -74,11 +74,16 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'real_estate',
+        'USER': 'pks',
+        'PASSWORD': 'abc123!',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
